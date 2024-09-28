07962abec4615fd838637edbf14cfe49afcd210d
Brian <briancmuhammad@yahoo.com>
2024-09-28 07:14:44 -0700

    Added request package for calling api. Added apihelper file for calling api. Updated index.js and index.hbs for calling the api and adding the html elements

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const apiHelper = require('../helpers/apiHelper')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+apiHelper.callApi('https://satellite-rest-api.vercel.app/')
+.then(response => {
+  console.log(response);
+  res.render('index', {title: "Satellites", data: response});
+})
+.catch(error => {
+  res.send(error)
+})
+
 });
 
 module.exports = router;
